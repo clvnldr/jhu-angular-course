0d33c6baf18634a0ae7577bc6a61a686143567cf
Calvin Leader <Calvin.Leader@jhuapl.edu>
2017-10-05 17:48:51 -0400

    Implemented baseline requirements for module 1 assignment

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -3,7 +3,25 @@
 
 angular.module('lunchCheck', [])
 
-.controller('LunchCheckController', function() {
+.controller('LunchCheckController', function($scope) {
+
+  $scope.lunchItems = "";
+  $scope.message= "";
+
+  $scope.checkLunchItems = function(lunchItems) {
+    if (lunchItems === "") {
+      $scope.message = "Please enter data first";
+    } else {
+      var numLunchItems = lunchItems.split(",").length;
+
+      if (numLunchItems <= 3) {
+        $scope.message = "Enjoy!";
+      } else {
+        $scope.message = "Too much!";
+      }
+      
+    }
+  };
 
 });
 
