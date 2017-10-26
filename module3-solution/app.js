f4f1cb3cf79fe073e5381d82f385ba05f49d932a
Calvin Leader <Calvin.Leader@jhuapl.edu>
2017-10-26 17:40:13 -0400

    Final implementation for module 3

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -4,16 +4,19 @@
 angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
 .service('MenuSearchService', MenuSearchService)
-.directive('foundItems', FoundItems);
+.directive('foundItems', FoundItemsDirective);
 
-function FoundItems() {
+function FoundItemsDirective() {
   var ddo = {
     templateUrl: 'foundItems.html',
     scope: {
       items: '<',
       onRemove: '&'
-    }
-  }
+    },
+    controller: NarrowItDownController,
+    controllerAs: 'narrowItDown',
+    bindToController: true
+  };
 
   return ddo;
 }
@@ -21,10 +24,20 @@ function FoundItems() {
 NarrowItDownController.$inject = ['MenuSearchService'];
 function NarrowItDownController(MenuSearchService) {
   var vm = this;
-  vm.found = [];
 
   vm.getMatchedMenuItems = function(searchTerm) {
-    vm.found = MenuSearchService.getMatchedMenuItems(searchTerm);
+    var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+
+    promise.then(function (response) {
+      vm.found = response;
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+  };
+
+  vm.removeItem = function(itemIndex) {
+    vm.found.splice(itemIndex, 1);
   };
 }
 
@@ -33,20 +46,23 @@ function MenuSearchService($http) {
   var service = this;
 
   service.getMatchedMenuItems = function(searchTerm) {
-    // http request and return result after looping through errything
     return $http({
       url: "https://davids-restaurant.herokuapp.com/menu_items.json"
     }).then(function (result) {
       var foundItems = [];
-      var menuItems = result.data.menu_items;
 
-      menuItems.forEach(function (menuItem) {
-        if (menuItem.description.includes(searchTerm)) {
-          foundItems.push(menuItem);
-        }
-      });
+      if (searchTerm === "") {
+        return foundItems;
+      } else {
+        var menuItems = result.data.menu_items;
 
-      return foundItems;
+        menuItems.forEach(function (menuItem) {
+          if (menuItem.description.includes(searchTerm)) {
+            foundItems.push(menuItem);
+          }
+        });
+        return foundItems;
+      }
     });
 
   };
