caae2f24e3c07007d79273fab5555dd20b821f25
Calvin Leader <Calvin.Leader@jhuapl.edu>
2017-10-31 18:01:09 -0400

    Begin adding data module and menudata service

---
+++ module4-solution/src/data/menudata.service.js
@@ -0,0 +1,12 @@
+(function() {
+'use strict';
+
+angular.module('Data')
+.service('MenuDataService', MenuDataService);
+
+MenuDataService.$inject = ['$http'];
+function MenuDataService($http) {
+  var service = this;
+}
+
+})();
